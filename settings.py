aea819ee325ec1b97fa337188007bfd30ae517e1
Your Name <you@example.com>
2023-10-23 19:01:07 +0300

    1commit

--- settings.py
+++ settings.py
@@ -1,15 +1,13 @@
 import os
 from dataclasses import dataclass
-
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 
 @dataclass
 class Settings:
-    DATABASE_NAME = os.getenv('DATABASE_NAME', '')
+    DATABASE_NAME = os.getenv('DATABASE', '')
     DATABASE_USER = os.getenv('DATABASE_USER', '')
     DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '')
     DATABASE_HOST = os.getenv('DATABASE_HOST', '')
