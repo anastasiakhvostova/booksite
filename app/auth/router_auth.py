9610e8e3ac4b6b8ad7a8d5b4fe16e7a6a0fd559e
Your Name <you@example.com>
2023-10-31 14:02:34 +0200

    9commit

--- app/auth/router_auth.py
+++ app/auth/router_auth.py
@@ -14,13 +14,13 @@ router = APIRouter(
 
 @router.post('/register', response_model=AuthRegistered, status_code=status.HTTP_201_CREATED)
 async def register_api(auth_details: AuthDetails):
-    await dao.create_user(
+    user_data = await dao.create_user(
         name=auth_details.name,
         login=auth_details.login,
         password=auth_details.password,
         notes=auth_details.notes,
     )
-    return AuthRegistered(success=True, id=5656, login='hfgffd@ukr.net')
+    return AuthRegistered(success=True, id=user_data[0], login='hfgffd@ukr.net')
 
 # @router.post('/register', response_model=AuthRegistered, status_code=status.HTTP_201_CREATED)
 # async def register_api(request: Request, response: Response, auth_details: AuthDetails):
